f54211516fd40e48db54ed7b3f52a88d3306db17
Branca Chiotte <brancachiotte@gmail.com>
2020-12-03 17:18:53 +0000

    continue working

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: "Click me!" };
+  const labelText = "Enter name: " ;
+
+  return (
+    <div>
+      <label className="lable" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
-  // same but without arrow function
+// same but without arrow function
 // const App = function () {
 //   return <div>Hi there!</div>;
 // };
 
-  // Take the react component and show it on the screen
-ReactDOM.render(
-  <App />,
-  document.querySelector("#root")
-);
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
